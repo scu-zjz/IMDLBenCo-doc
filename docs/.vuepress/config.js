08904940ae94df7feb26d35468a4d45d30b6297e
Ma, Xiaochen <mxch1122@126.com>
2024-06-15 03:23:26 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: 'IMDLBenCo-doc',
   lang: 'en-US',
 
   title: 'VuePress',
